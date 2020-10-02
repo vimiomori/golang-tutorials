2d040cbced1696c819237c6ae7344b56adaba267
vimiomori <vivian.muchen@gmail.com>
2020-10-02 22:37:26 +0900

    finish cards tutorial

---
+++ cards/deck_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 16 {
+		t.Errorf("Expected length of 16, but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
+	} 
+
+	if d[len(d) - 1] != "Four of Clubs" {
+		t.Errorf("Expected last card of Four of Clubs, but got %v", d[len(d) - 1])
+	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	fileName := "_decktesting"
+	os.Remove(fileName)
+	deck := newDeck()
+	deck.saveToFile(fileName)
+
+	loadedDeck := newDeckFromFile(fileName)
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, got %v", len(loadedDeck))
+	}
+
+	os.Remove(fileName)
+}
+
+// func TestShuffle(t *testing.T) {
+
+// }
